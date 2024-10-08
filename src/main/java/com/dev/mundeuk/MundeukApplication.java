c8736dfc25fc7551a62b21b084019712424346e8
yy-ham <yyham.dev@gmail.com>
2024-10-08 21:55:49 +0900

    Initial commit

---
+++ src/main/java/com/dev/mundeuk/MundeukApplication.java
@@ -0,0 +1,13 @@
+package com.dev.mundeuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundeukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundeukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/mundeuk/MundeukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.mundeuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundeukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
