f597198107197a88e3524cf8207f05a16ae32d04
yy-ham <yyham.dev@gmail.com>
2024-10-13 23:35:59 +0900

    feat(member): 회원가입 기능 구현
    - MemberController 생성
    - MainController 생성
    - MemberDto에 Validation 어노테이션 추가
    - Validation 의존성 추가
    - 메인 페이지 추가
    - 회원가입 페이지 추가
    - 에러 페이지 추가
    
    Related to #1

---
+++ src/main/java/com/dev/mundeuk/domain/member/controller/MemberController.java
@@ -0,0 +1,53 @@
+package com.dev.mundeuk.domain.member.controller;
+
+import com.dev.mundeuk.domain.member.dto.MemberDto;
+import com.dev.mundeuk.domain.member.entity.Member;
+import com.dev.mundeuk.domain.member.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/join")
+    public String join(Model model) {
+        model.addAttribute("memberDto", new MemberDto());
+        return "member/join";
+    }
+
+    @PostMapping("/join")
+    public String join(@Valid MemberDto memberDto, BindingResult bindingResult, Model model) {
+
+        if(bindingResult.hasErrors()) {
+            System.out.println(bindingResult.toString());
+            System.out.println("-------");
+            System.out.println(bindingResult.getFieldError().getDefaultMessage());
+            return "member/join";
+        }
+
+        try {
+            System.out.println(memberDto);
+            Member member = Member.toEntity(memberDto, passwordEncoder);
+            memberService.join(member);
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            System.out.println(e.getMessage());
+            return "member/join";
+        }
+
+        return "redirect:/";
+    }
+
+}
--- src/main/java/com/dev/mundeuk/domain/member/dto/MemberDto.java
+++ src/main/java/com/dev/mundeuk/domain/member/dto/MemberDto.java
@@ -1,7 +1,12 @@
 package com.dev.mundeuk.domain.member.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDate;
 
@@ -9,8 +14,18 @@ import java.time.LocalDate;
 @Setter
 public class MemberDto {
 
-    private String id;
-    private String password;
+    @NotBlank(message = "이름을 입력해주세요.")
+    @Length(min = 2, message = "이름은 최소 2자리 이상이어야 합니다.")
     private String name;
+
+    @NotEmpty(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
+    private String id;
+
+    @NotEmpty(message = "비밀번호를 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()])[a-zA-Z\\d!@#$%^&*()]{8,16}$",
+            message = "비밀번호는 영문, 숫자, 특수문자를 조합하여 8자 이상 16자 이하이어야 합니다.")
+//    @Length(min = 8, max = 16, message = "비밀번호는 8자이상 16자 이하로 입력해주세요.")
+    private String password;
 
 }
