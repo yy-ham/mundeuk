6c8d05da613ee29c8b686556f4f6fcf2219c9156
yy-ham <yyham.dev@gmail.com>
2024-10-19 15:24:36 +0900

    feat(member): SecurityConfig 메소드 추가
    - filterChain 메소드 추가
    - authenticationManager 메소드 추가
    
    Related to #2

--- src/main/java/com/dev/mundeuk/common/SecurityConfig.java
+++ src/main/java/com/dev/mundeuk/common/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.dev.mundeuk.common;
 
+import com.dev.mundeuk.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,16 +14,34 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final MemberService memberService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin((form) -> form
+                .loginPage("/member/login")
+                .usernameParameter("id")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/")
+                .failureUrl("/member/login/error")
+        );
+
+        http.userDetailsService(memberService);
+
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
